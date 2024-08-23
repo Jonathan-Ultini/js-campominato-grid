7618512d17d0033567e6ace6ef1f0686216cb7fd
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-23 14:52:33 +0200

    milestone 3-4

--- js/script.js
+++ js/script.js
@@ -9,12 +9,19 @@ playButton.addEventListener('click', function () {
 
 // Funzione per generare la griglia
 function generateGrid() {
-  gridContainer.innerHTML = '';
+  gridContainer.innerHTML = ''; // Puliamo il contenitore della griglia
 
   for (let i = 1; i <= 100; i++) {
     const cell = document.createElement('div');
     cell.classList.add('grid-item');
     cell.innerHTML = i;
+
+    // Aggiungiamo l'evento di click su ogni cella
+    cell.addEventListener('click', function () {
+      cell.classList.add('clicked');
+      console.log(`Hai cliccato sulla cella numero: ${i}`);
+    });
+
     gridContainer.appendChild(cell);
   }
 }
